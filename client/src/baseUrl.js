624b4191e077b656d865fcc2a5c7600a1c2b7127
ali.mis <ali.mis@stmu.edu.pk>
2023-11-20 15:17:06 +0500

    server baseurl

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000";
-//export const BASE_URL = "http://124.109.47.58:8000";
+//export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "http://124.109.47.58:8000";
