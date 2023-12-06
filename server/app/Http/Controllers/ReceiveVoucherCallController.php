3a259c1c0d6121e3587a26290d09923c916a0de0
Muhammad Ali <ali.mis@stmu.edu.pk>
2023-12-06 10:42:54 +0500

    Fee receieive conditions from both table

--- server/app/Http/Controllers/ReceiveVoucherCallController.php
+++ server/app/Http/Controllers/ReceiveVoucherCallController.php
@@ -4,46 +4,49 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\BankVoucherInfo;
+use App\Models\Upload;
 
 class ReceiveVoucherCallController extends Controller
 {
-    //
-    public function ReceiveFeeVoucher(Request $request){
+    public function ReceiveFeeVoucher(Request $request)
+    {
+        // Check if the voucher ID exists in the upload table
+        $uploadedVoucher = Upload::where('challan_generation_id', $request->voucher_id)->first();
 
+        if (!$uploadedVoucher) {
+            return response()->json(['Code:5 Message' => 'Invalid Voucher Number'], 400);
+        }
 
-        $student_info_in_bank_voucher_table = BankVoucherInfo::where('challan_generation_id', $request->voucher_id)->get();
+        // Check if the voucher has been paid
+        $bankVoucher = BankVoucherInfo::where('challan_generation_id', $request->voucher_id)->first();
 
-        if (!$student_info_in_bank_voucher_table->isEmpty()) {
-            $voucher = $student_info_in_bank_voucher_table->first(); // Retrieve the first model instance from the collection
-        
-            if ($voucher['paid_status'] == 1) {
+        if ($bankVoucher) {
+            if ($bankVoucher['paid_status'] == 1) {
                 return response()->json(['Code:2 Message' => 'Voucher is already Paid!'], 400);
             }
-        
-            if ($voucher['paid_status'] == 0 && $voucher['challan_generation_id'] == $request->voucher_id) {
-                if (
-                    $voucher['amount'] == $request->amount &&
-                    $voucher['fine'] == $request->fine &&
-                    $voucher['net_amount'] == $request->net_amount
-                ) {
-                    if ($voucher['customer_id'] == $request->customer_id) {
-                        $voucher->paid_status = 1;
-                        $voucher->instrumentNo = $request->instrument_no;
-                        $voucher->receipt_date = $request->receipt_date;
-        
-                        $voucher->save();
-        
-                        return response()->json(['Code:1 Message' => 'Voucher paid successfully!'], 201);
-                    } else {
-                        return response()->json(['Code:3 Message' => 'Invalid Customer Id'], 401);
-                    }
+
+            // Compare other fields to ensure correctness
+            if (
+                $bankVoucher['amount'] == $request->amount &&
+                $bankVoucher['fine'] == $request->fine &&
+                $bankVoucher['net_amount'] == $request->net_amount
+            ) {
+                if ($bankVoucher['customer_id'] == $request->customer_id) {
+                    $bankVoucher->paid_status = 1;
+                    $bankVoucher->instrumentNo = $request->instrument_no;
+                    $bankVoucher->receipt_date = $request->receipt_date;
+
+                    $bankVoucher->save();
+
+                    return response()->json(['Code:1 Message' => 'Voucher paid successfully!'], 201);
                 } else {
-                    return response()->json(['Code:4 Message' => 'Please Enter Correct Amounts Like Amount, Fine, and Net Amount'], 400);
+                    return response()->json(['Code:3 Message' => 'Invalid Customer Id'], 401);
                 }
+            } else {
+                return response()->json(['Code:4 Message' => 'Please Enter Correct Amounts Like Amount, Fine, and Net Amount'], 400);
             }
         } else {
-            return response()->json(['Code:5 Message' => 'Invalid Voucher Number which you processed'], 400);
+            return response()->json(['Code:5 Message' => 'Fee Not paid yet'], 400);
         }
-        
     }
 }
